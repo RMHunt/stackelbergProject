58522d6f731132098ca94eb017eaec1bfedf5ef6
ivan fernando <ivan fernando>
2016-03-26 22:16:59 +0700

    maximiser prototype

--- src/Maximiser.java
+++ src/Maximiser.java
@@ -26,6 +26,26 @@ class Maximiser
     }
     
     /**
-     * Calculate maximum for given function;
+     * Calculate maximum for given function. The values are hard coded due to the
+     * nature of the coursework.
      */
+    private double calculateMaximum()
+    {
+        // a, b, and c for the quadratic equation
+        // These calculations are derived from the given function in the instructions.
+        double quadA = 0.3 * reactionFunction.getB() - 1;
+        double quadB = 0.3 * reactionFunction.getA() - quadA + 2;
+        double quadC = 2 + 0.3 * reactionFunction.getA();
+        
+        double vertex = (0 - quadB) / 2 * quadA;
+        
+        // Check if vertex is maximum or minimum
+        if((2 * quadA * (vertex + 0.1) + quadB) < 0)
+        {
+            // if maximum
+            return vertex;
+        } else {
+            return MAX_DOUBLE;
+        }
+    }
 } 
